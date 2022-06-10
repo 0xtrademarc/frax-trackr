264808daa94b68e073f9ae04016cb0e2de33edc8
0xtrademarc <marcusloo1338@gmail.com>
2022-06-10 13:39:50 +0100

    initial commit

---
+++ src/components/Dashboards/dashboards.js
@@ -0,0 +1,33 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+const Dashboards = () => {
+    return (   
+        <div className = "dashboard-container">
+            <div className = "dashboard">
+                <div> DASHBOARD TITLE </div>
+                <div> DASHBOARD DATA/CHART</div>
+            </div>
+
+            <div className = "dashboard">
+                <div> DASHBOARD TITLE </div>
+                <div> DASHBOARD DATA/CHART</div>
+            </div>
+
+            <div className = "dashboard">
+                <div> DASHBOARD TITLE </div>
+                <div> DASHBOARD DATA/CHART</div>
+            </div>
+
+            <div className = "dashboard">
+                <div> DASHBOARD TITLE </div>
+                <div> DASHBOARD DATA/CHART</div>
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default Dashboards;
